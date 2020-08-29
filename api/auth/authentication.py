93919d9d81532e1cb88e78089d260ff666359303
Ogollah <kogollah2011@gmail.com>
2020-08-29 09:49:01 +0300

    Add user sign up and login authentication

--- api/auth/authentication.py
+++ api/auth/authentication.py
@@ -3,10 +3,13 @@ import os
 import datetime
 from flask import json
 from ..model.user import UserModel
+from flask import json, Response, request, g
+from functools import wraps
 
 class Auth():
+
     """
-    Authentication class
+    Authentication class.
     """
 
     @staticmethod
@@ -19,13 +22,13 @@ class Auth():
             }
             return jwt.encode(
                 payload,
-                os.getenv(JWT_SECRET_KEY),
+                os.getenv('JWT_SECRET_KEY'),
                 'HS256'
             ).decode("utf-8")
         except Exception as e:
             return Response(
                 mimetype="application/json",
-                response=json.dumps({'error': 'error in generating user token'}),
+                response=json.dumps({'error': 'error in generating user token {}'.format(e)}),
                 status=400
             )
 
@@ -37,8 +40,8 @@ class Auth():
             payload = jwt.decode(token, os.getenv('JWT_SECRET_KEY'))
             re['data'] = {'user_id': payload['sub']}
             return re
-        except jwt.ExpiredSignatureError as e1:
-            re['error'] = {'message': 'token expired, please login again'}
+        except jwt.ExpiredSignatureError as e:
+            re['error'] = {'message': 'token expired, please login again {}'.format(e)}
             return re
         except jwt.InvalidTokenError:
             re['error'] = {'message': 'Invalid token, please try again with a new token'}
